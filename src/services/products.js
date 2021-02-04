be675cb2e20e1c4177e295397a2aad6208c3aa3a
noorarytila <rytilanoora@gmail.com>
2021-02-04 17:16:24 +0200

    modified app to get data from custom api

--- src/services/products.js
+++ src/services/products.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
-const baseUrl = 'https://bad-api-assignment.reaktor.com/v2/products'
+const baseUrl = 'http://localhost:5000/products'
 
 const getAll = async (product) => {
 
     try {
         const request = axios.get(`${baseUrl}/${product}`)
         const response = await request
-        return response.data
+        console.log(response.data.products)
+        return response.data.products
     } catch (error) {
         console.log(error)
     }
