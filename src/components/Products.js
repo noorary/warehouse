46ef19e8e60fea265611e604508e594d95101c9f
noorarytila <rytilanoora@gmail.com>
2021-02-21 18:46:14 +0200

    New url for api, few UI fixes

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,12 +7,27 @@ import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.m
 
 const Product = ({ products }) => {
 
+  function stockFormatter(cell, row) {
+    if (row.instock === 'INSTOCK') {
+      return (
+        <span>
+          <strong style={ { color: 'green' } }>$ { cell }</strong>
+        </span>
+      )
+    }
+  
+    return (
+      <span>$ { cell } </span>
+    );
+  }
+
   const columns = [
-    { dataField: 'type', text: 'Type'},
-    { dataField: 'name', text: 'Name'},
-    { dataField: 'color', text: 'Color'},
-    { dataField: 'price', text: 'Price'},
-    { dataField: 'manufacturer', text: 'Manufacturer'}
+    { dataField: 'type', text: 'Type' },
+    { dataField: 'name', text: 'Name' },
+    { dataField: 'color', text: 'Color' },
+    { dataField: 'price', text: 'Price' },
+    { dataField: 'manufacturer', text: 'Manufacturer' },
+    { dataField: 'instock', text: 'In stock'}
   ]
 
   const pagination = paginationFactory({
--- src/services/products.js
+++ src/services/products.js
@@ -1,17 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios'
-const baseUrl = 'http://localhost:5000/products'
+const baseUrl = 'https://nr-warehouse-api.herokuapp.com/products'
 
 const getAll = async (product) => {
 
-    try {
-        const request = axios.get(`${baseUrl}/${product}`)
-        const response = await request
-        console.log(response.data.products)
-        return response.data.products
-    } catch (error) {
-        console.log(error)
-    }
-    
+  try {
+    const request = axios.get(`${baseUrl}/${product}`)
+    const response = await request
+    console.log(response.data.products)
+    return response.data.products
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 export default { getAll }
