9c4e343c35b7f58ceae2a5040c0d2b6409a67693
noorarytila <rytilanoora@gmail.com>
2021-02-22 17:00:50 +0200

    back to heroku api

--- src/services/products.js
+++ src/services/products.js
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios'
-const baseUrl = 'http://nr-warehouse-api-dev.eu-central-1.elasticbeanstalk.com/products'
+const baseUrl = 'https://nr-warehouse-api.herokuapp.com/products'
 
 const getAll = async (product) => {
 
   try {
     const request = axios.get(`${baseUrl}/${product}`)
     const response = await request
-    console.log(response.data.products)
     return response.data.products
   } catch (error) {
     console.log(error)
